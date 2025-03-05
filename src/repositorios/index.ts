500069986f5503ef40120c043f2c8fd04799af04
Maycon Oliveira <mayconbrito1998@hotmail.com>
2025-03-04 21:13:56 -0300

    up

--- src/repositorios/index.ts
+++ src/repositorios/index.ts
@@ -1,4 +1,6 @@
+import * as feriado from './feriado';
+import * as justificativa from './justificativa';
 import * as ponto from './ponto';
 import * as usuario from './usuario';
 
-export const Repositorios = { ...usuario, ...ponto };
+export const Repositorios = { ...usuario, ...ponto, ...justificativa, ...feriado };
